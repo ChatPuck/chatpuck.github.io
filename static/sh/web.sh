a5f844b0e1991e76b9573ce8ce089ba2177a2902
Puck <0xf197@gmail.com>
2025-05-25 00:00:02 +0800

    Update web.sh

--- static/sh/web.sh
+++ static/sh/web.sh
@@ -1,49 +1,67 @@
 #!/bin/bash
 
-# 定义目标目录
-TARGET_DIR="/home/www/"
+# 定义目录路径
+BASE_DIR="/data/web/sites/puck.host/www/"
+REPO_DIR="$BASE_DIR/chatpuck.github.io"
 
-# 切换到目标目录
-echo "切换到目标目录: $TARGET_DIR"
-cd $TARGET_DIR || { echo "切换目录失败"; exit 1; }
+# 安装 git
+yum install -y git
 
-# 检查目标目录是否为空
-if [ "$(ls -A $TARGET_DIR)" ]; then
-    # 如果目录不为空，自动删除目标目录中的所有文件
-    echo "目标目录 $TARGET_DIR 不为空，删除其中的所有文件..."
-    rm -rf ${TARGET_DIR}*
+# 克隆仓库到指定目录
+git clone https://github.com/ChatPuck/chatpuck.github.io.git $REPO_DIR
+
+# 进入仓库目录
+cd $REPO_DIR
+
+# 移动 docs 目录下的所有文件到与 chatpuck.github.io 同级目录
+if [ -d "$REPO_DIR/docs" ]; then
+    mv $REPO_DIR/docs/* $BASE_DIR/
+    echo "已将 docs 目录下的所有文件移动到与 chatpuck.github.io 同级目录"
+else
+    echo "docs 目录不存在"
 fi
 
-# 再次确认我们在正确的目录中
-echo "当前目录: $(pwd)"
-
-# 安装 wget 和 unzip 工具（如果尚未安装）
-echo "安装 wget 和 unzip 工具..."
-sudo yum install -y wget unzip
-
-# 下载 GitHub 仓库的 ZIP 文件
-echo "下载 GitHub 仓库的 ZIP 文件..."
-wget https://github.com/feeday/WebPage/archive/refs/heads/main.zip -O WebPage-main.zip
-
-# 检查下载是否成功
-if [ ! -f "WebPage-main.zip" ]; then
-    echo "下载失败"
-    exit 1
+# 移动 docs/post 目录下的所有文件到与 chatpuck.github.io 同级的 post 目录
+if [ -d "$REPO_DIR/docs/post" ]; then
+    shopt -s nullglob
+    files=($REPO_DIR/docs/post/*)
+    if [ ${#files[@]} -gt 0 ]; then
+        mv $REPO_DIR/docs/post/* $BASE_DIR/post/
+        echo "已将 docs/post/ 中的所有文件移动到 post 目录"
+    else
+        echo "docs/post/ 目录为空，无需移动"
+    fi
+    shopt -u nullglob
+else
+    echo "docs/post 目录不存在"
 fi
 
-# 解压下载的 ZIP 文件
-echo "解压 ZIP 文件..."
-unzip WebPage-main.zip
-
-# 检查解压是否成功
-if [ ! -d "WebPage-main" ]; then
-    echo "解压失败"
-    exit 1
+# 将 backup/backup 文件夹移动到与 chatpuck.github.io 同级目录
+if [ -d "$REPO_DIR/backup" ]; then
+    mv $REPO_DIR/backup $BASE_DIR/
+    echo "已将 backup/backup 文件夹移动到与 chatpuck.github.io 同级目录"
+else
+    echo "backup/backup 文件夹不存在"
 fi
 
-# 移动解压后的子文件夹内容到当前目录
-mv WebPage-main/* .
-rm -rf WebPage-main.zip WebPage-main
+# 删除 chatpuck.github.io 文件夹
+cd $BASE_DIR
+rm -rf $REPO_DIR
 
-echo "项目解压完成"
+# 删除 index.html 文件（如果存在）
+if [ -f "$BASE_DIR/index.html" ]; then
+    rm $BASE_DIR/index.html
+    echo "删除 index.html 文件"
+else
+    echo "index.html 文件不存在"
+fi
 
+# 将 pc.html 重命名为 index.html（如果存在）
+if [ -f "$BASE_DIR/pc.html" ]; then
+    mv $BASE_DIR/pc.html $BASE_DIR/index.html
+    echo "将 pc.html 重命名为 index.html"
+else
+    echo "pc.html 文件不存在"
+fi
+# 完成
+echo "操作完成，chatpuck.github.io 文件夹已删除！"
